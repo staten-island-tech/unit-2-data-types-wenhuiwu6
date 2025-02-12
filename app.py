9d26467d3ae7ef7c894386ed7453612c316e9aa7
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-02-12 09:24:53 -0500

    function

--- app.py
+++ app.py
@@ -1,34 +1,46 @@
-""" x = 3
-y = float(3)
-print(x,y)
-
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print(i)
- """
-
-""" day_of_week = input("what day is it? ")
-if day_of_week == "Friday":
-    print("correct")
-else:
-    print("incorrect") """
-
 """ number=7
 if number % 2 == 0:
     print("even")
 else:
     print("odd") """
 
-service= 
-"bad": 0
-"okay": 0.15
-"good": 0.20
-"great": 0.25
+service = "good"
+bill= 400
+
+def calculate_tip(bill_amount, service_quality):
+    tip_percentages = {
+        'bad': 0,
+        'okay': 0.15,
+        'good': 0.20,
+        'great': 0.25
+}
 
 
-service=()
 
+
+tip_amount = bill_amount * tip_percentage
+total_amount = bill_amount + tip_amount
+
+result = calculate_tip(bill, service)
+
+
+""" def calculate_tip(bill_amount, service_quality):   
+    if service_quality == ("bad") :
+        print = 0.0
+    elif service_quality == ("okay") :
+        print = 0.15
+    elif service_quality == ("good") :
+        print = 0.20
+    elif service_quality == ("great") :
+        print = 0.25
+ """
+
+
+"""         "bad": 0.0,
+        "okay": 0.15,
+        "good": 0.20,
+        "great": 0.25
+ """
 
 
  
