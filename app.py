2fcc584b874cdb805ec46b9551129dd6691990f8
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-02-13 09:22:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,36 +4,52 @@ if number % 2 == 0:
 else:
     print("odd") """
 
-service = "good"
+""" x = "test"
+print(f"hello {x}")
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
 bill= 400
-
-def calculate_tip(bill_amount, service_quality):
-    tip_percentages = {
-        'bad': 0,
-        'okay': 0.15,
-        'good': 0.20,
-        'great': 0.25
-}
-
-
-
-
-tip_amount = bill_amount * tip_percentage
-total_amount = bill_amount + tip_amount
-
-result = calculate_tip(bill, service)
-
-
-""" def calculate_tip(bill_amount, service_quality):   
-    if service_quality == ("bad") :
+print("how was the service")
+service= "good"
+if service == ("bad") :
         print = 0.0
-    elif service_quality == ("okay") :
+elif service == ("okay") :
         print = 0.15
-    elif service_quality == ("good") :
+elif service == ("good") :
         print = 0.20
-    elif service_quality == ("great") :
-        print = 0.25
+elif service == ("great") :
+        print = 0.25  
+else: 
+        print("invalid") 
+        
+
+
+
+""" tip_amount = bill_amount * tip_percentage
+total_amount = bill_amount + tip_amount
+result = calculate_tip(bill, service)
  """
+
+
+
+""" (bill_amount, service_quality):
+    tip_percentages:
+    'bad': 0
+    'okay': 0.15
+    'good': 0.20
+    'great': 0.25
+
+
+ """
+
+    
+ 
 
 
 """         "bad": 0.0,
