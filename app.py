a065f89ad3ba964db2e4c6b135ec7916d507a333
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-02-14 09:06:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,48 +15,16 @@ else:
     print('cold') """
 
 bill= 400
-print("how was the service")
 service= "good"
-if service == ("bad") :
-        print = 0.0
-elif service == ("okay") :
-        print = 0.15
-elif service == ("good") :
-        print = 0.20
-elif service == ("great") :
-        print = 0.25  
-else: 
+def tax(service):
+    if service == ("bad") :
+        print("0.0")
+    elif service == ("okay") :
+        print("0.15")
+    elif service == ("good") :
+        print("0.20")
+    elif service == ("great") :
+        print("0.25")  
+    else: 
         print("invalid") 
         
-
-
-
-""" tip_amount = bill_amount * tip_percentage
-total_amount = bill_amount + tip_amount
-result = calculate_tip(bill, service)
- """
-
-
-
-""" (bill_amount, service_quality):
-    tip_percentages:
-    'bad': 0
-    'okay': 0.15
-    'good': 0.20
-    'great': 0.25
-
-
- """
-
-    
- 
-
-
-"""         "bad": 0.0,
-        "okay": 0.15,
-        "good": 0.20,
-        "great": 0.25
- """
-
-
- 
