588fd96c8782f40571a3f62a99218fb723f2b706
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-02-24 09:25:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,22 @@ if number % 2 == 0:
 else:
     print("odd") """
 
-""" x = "test"
-print(f"hello {x}")
-temp = 75
-if temp > 68:
-    print('warm')
-elif temp == 68:
-    print('perfect')
-else:
-    print('cold') """
-
-bill= 400
+""" bill= 400
 service= "good"
-def tax(service):
-    if service == ("bad") :
-        print("0.0")
-    elif service == ("okay") :
-        print("0.15")
-    elif service == ("good") :
-        print("0.20")
-    elif service == ("great") :
-        print("0.25")  
-    else: 
-        print("invalid") 
-        
+if service == ("bad") :
+    print("0.0")
+elif service == ("okay") :
+    print("0.15")
+elif service == ("good") :
+    print("0.20")
+elif service == ("great") :
+    print("0.25")  
+else: 
+    print("invalid") 
+ """
+
+""" number = 12
+for i in range (1, number +1): 
+    if(number % i== 0):
+        print(i) """
+
