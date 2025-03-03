16b4e0f44212ee889e0af9db1b90f43bc9e1d98f
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-03-03 09:26:37 -0500

    Create ATM.py

---
+++ ATM.py
@@ -0,0 +1,14 @@
+balance= 500
+withdraw=input("Would you want to withdraw money: ")
+
+while balance != withdraw:
+    def balance(withdraw):
+        if withdraw<=balance== True:
+            return (new_balance)
+        new_balance= balance - withdraw 
+        if withdraw>balance== False:
+            return("insufficient funds so try again")
+        
+        
+        
+print("your new balance is: ", )
